1b068116e8c5d78ee9fe3bc18241d443d10c5428
Jordan <jordanwinther@hotmail.com.br>
2022-05-14 21:31:32 -0400

    Web Security Cors add

---
+++ wintherMovie/src/main/java/com/winther/movie/WintherMovieApplication.java
@@ -0,0 +1,13 @@
+package com.winther.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WintherMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WintherMovieApplication.class, args);
+	}
+
+}
---
+++ wintherMovie/src/test/java/com/winther/movie/WintherMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winther.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WintherMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
